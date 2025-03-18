96a7b03bba84cc5c113d98f195c182c64406c8cd
byetiskin <begum.yetiskin@inonu.edu.tr>
2025-03-18 13:41:13 +0300

    Add files via upload

---
+++ hafta7/uyg1.c
@@ -0,0 +1,11 @@
+// Sentaks hata örnek
+
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    x = 10;
+    y = 20
+    printf("Toplam: %d\n", x + y);
+    return 0;
+}
---
+++ hafta7/uyg2.c
@@ -0,0 +1,10 @@
+// Semantik hata örnek
+
+#include <stdio.h>
+
+int main() {
+    int x;
+    x = "Merhaba";  // Hatalı veri tipi ataması
+    printf("%d\n", x);
+    return 0;
+}
